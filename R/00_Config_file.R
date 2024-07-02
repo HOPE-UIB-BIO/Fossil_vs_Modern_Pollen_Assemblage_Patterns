29e97d37a02a3930aa2cc4eeed19e3a779f30809
Kuber Bhatta <47561250+kbh022@users.noreply.github.com>
2024-07-02 11:19:34 +0200

    commit: configuration

--- R/00_Config_file.R
+++ R/00_Config_file.R
@@ -1,19 +1,11 @@
 #----------------------------------------------------------#
 #
 #
-#                       Project name
+#         Latitudinal analysis of phylogenetic dispersion
 #
 #                       Config file
-#                 
-#
-#                         Names 
-#                         Year
-#
+#                          
 #----------------------------------------------------------#
-# Configuration script with the variables that should be consistent throughout 
-#   the whole repo. It loads packages, defines important variables, 
-#   authorises the user, and saves config file.
-
 
 #----------------------------------------------------------#
 # 1. Load packages -----
@@ -25,112 +17,181 @@ if(!exists("update_repo_packages")){
 
 if(update_repo_packages == TRUE){
   
-  # install fossilpol from github
-  if (!exists("already_installed_fossilpol")){
-    already_installed_fossilpol <- FALSE
-  }
-  
-  if(already_installed_fossilpol == FALSE){
-    devtools::install_github("HOPE-UIB-BIO/fossilpol",
-                             quiet = FALSE,
-                             upgrade = FALSE)
-    already_installed_fossilpol <- TRUE
-  }
-  
   if (!exists("already_synch")){
     already_synch <- FALSE
   }
   
   if(already_synch == FALSE){
     library(here)
-    # synchronise the package versions
+  # synchronise the package versions
     renv::restore(lockfile = here::here( "renv/library_list.lock"))
     already_synch <- TRUE
     
-    # save snapshot of package versions
-    # renv::snapshot(lockfile =  "renv/library_list.lock")  # do only for update
+  # save snapshot of package versions
+    renv::snapshot(lockfile =  "renv/library_list.lock")  # do only for update
   }
 }
 
 # define packages
+
 package_list <- 
   c(
+    "ape",
     "assertthat",
+    "car",
+    "corrplot", 
+    "cowplot",
     "devtools",
-    "fossilpol",
-    "here",      
-    "renv",       
-    "roxygen2",   
+    "dplyr",
+    "flextable",
+    "ggplot2",
+    "ggpubr",
+    "ggmap",
+    "ggspatial",
+    "ggtext",
+    "gratia",
+    "grid",
+    "gridExtra",
+    "here",   
+    "leaflet",
+    "lemon", 
+    "maps",
+    "mgcv", 
+    "multidplyr",
+    "parallel",
+    "performance",
+    "picante",
+    "raster",
+    "RColorBrewer",
+    "REcopol",
+    "renv", 
+    "RFossilpol",
+    "rpart",
+    "roxygen2", 
+    "scales",
+    "sf",
+    "sjmisc",
+    "sjstats", # For use for r2 functions
+    "targets",
     "tidyverse",  
-    "usethis"   
+    "usethis",
+    "vegan",
+    "viridis",
+    "xaringan"
   )
 
 # load all packages
 sapply(package_list, library, character.only = TRUE)
 
-
 #----------------------------------------------------------#
 # 2. Define space -----
 #----------------------------------------------------------#
-
-current_date <- Sys.Date()
-
 # project directory is set up by 'here' package, Adjust if needed 
 current_dir <- here::here()
 
-
 #----------------------------------------------------------#
-# 3. Load functions -----
+# 3. Source functions -----
 #----------------------------------------------------------#
-
-# get vector of general functions
-fun_list <- 
-  list.files(
-    path = "R/Functions/",
-    pattern = "*.R",
-    recursive = TRUE) 
-
-# source them
-sapply(
-  paste0("R/functions/", fun_list, sep = ""),
-  source)
+invisible(lapply(
+    list.files(
+      path = "R/functions/",
+      pattern = "*.R",
+      recursive = TRUE,
+      full.names = TRUE
+    ),
+    source)
+  )
 
 
 #----------------------------------------------------------#
-# 4. Authorise the user -----
+# 4. criteria for age limits of period of interest
 #----------------------------------------------------------#
+continents <- 
+  c("North America", "Europe", "Asia", "Latin America", "Africa", "Oceania")
 
-# if applicable
-
-#----------------------------------------------------------#
-# 5. Define variables -----
-#----------------------------------------------------------#
-
-
-#----------------------------------------------------------#
-# 6. Graphical options -----
-#----------------------------------------------------------#
-
-## examples
-#set ggplot output
-ggplot2::theme_set(
-  ggplot2::theme_classic())
-
-# define general
-text_size = 10
-line_size = 0.1
-
-# define output sizes
-image_width <- 16
-image_height <- 12
-image_units <- "cm"
-
-# define pallets
-
-# define common color
+continental_age_limits <- 
+  tibble(
+    continent = continents,
+    young_age = c(rep(1e3, 2), rep(1e3, 4)),
+    old_age = c(rep(8e3, 2), rep(6e3, 4)),
+    end_of_interest_period = c(rep(12e3, 6)))
 
 #----------------------------------------------------------#
-# 7. Save current config setting -----
+# 5. criteria to filter out levels (sample depths)
+#----------------------------------------------------------#
+min_n_levels <-  5  # at least 5 levels within period of interest 
+# (young_age to end_of_interest_period)
+
+min_n_grains <- 25 # each level at least 25 counted individuals 
+
+target_n_grains <- 150 # ideal number of counts
+
+percentage_samples <- 50 # threshold of number of samples with ideal counts
+
+maximum_age_extrapolation <- 3000 # how much age can be extrapolated beyond
+# the oldest chronology control point
+
+#----------------------------------------------------------#
+# 6. Colour scheme ----
+#----------------------------------------------------------#
+color_pd_curve <- "#0072B2"
+color_richness_curve <- "#D55E00"
+color_phylo_age_curve <- "#666666" 
+color_common <- "#000000" 
+  
+color_high_lat <- "#0099FF"
+color_low_lat <- "#FF0000"
+
+color_high_age <- "#009E73"
+color_low_age <- "#E69F00"
+
+# Assign unique colour to each climate zone
+my_palette <-
+  c("#FFCC99",
+    "#993300",
+    "#9999CC",
+    "#3399FF",
+    "#999999",
+    "#00CCCC",
+    "#99CC00",
+    "#006600",
+    "#996600"
+  ) %>% 
+  rlang::set_names(
+    nm = c(
+      "Arid",
+      "Cold-seasonally dry",
+      "Cold without dry season",
+      "Polar",
+      "Polar frost",
+      "Temperate",
+      "Tropical monsoon",
+      "Tropical rainforest",
+      "Tropical savannah"
+    )
+  )
+
+#----------------------------------------------------------#
+# 7. Authorise the user -----
 #----------------------------------------------------------#
 
-current_setting <- .extract.config.data()
+# Define directory for external storage for users
+auth_tibble <-
+  tibble::tribble(
+    ~name, ~path,
+    "ondrej", "C:/Users/ondre/My Drive/",
+    "vfe032", "G:/My Drive/",
+    "kuber", "H:/My Drive/"
+  )
+
+data_storage_path <-
+  auth_tibble %>%
+  dplyr::filter(name == Sys.info()["user"]) %>%
+  purrr::pluck("path")
+
+
+external_storage_targets <-
+  paste0(
+    data_storage_path,
+    "_targets_h1"
+  )
--- R/___Init_project___.R
+++ R/___Init_project___.R
@@ -1,14 +1,13 @@
 #----------------------------------------------------------#
+# Fossil pollen data can predict robust spatial patterns of biodiversity 
+#                        in the past
 #
-#
-#                     Project name
-#
-#                     Project setup
+#                       Project setup
 #                 
+#                         K. Bhatta 
 #
-#   O. Mottl, S. Flantua, K. Bhatta, V. Felde, A. Seddon 
-#                         2021
-#
+#                           2024
+#                         
 #----------------------------------------------------------#
 
 # Script to prepare all necessary components of environment to run the Project.
@@ -36,17 +35,38 @@ renv::deactivate()
 
 package_list <- 
   c(
+    "ape",
     "assertthat",
+    "cowplot",
     "devtools",
+    "ggpubr",
+    "ggmap",
+    "ggspatial",
+    "ggtext",
+    "glmm",
+    "glmmTMB",
+    "gratia",
+    "grid",
+    "gridExtra",
     "here",
     "httpgd",
     "janitor",
     "jsonlite",
     "languageserver",
+    "leaflet",
+    "lme4", 
+    "mgcv",
+    "performance",
+    "picante",
+    "raster",
+    "RColorBrewer",
+    "remotes",
     "renv",       
     "roxygen2",   
     "tidyverse",  
-    "usethis"   
+    "usethis",
+    "vegan",
+    "viridis"
   )
 
 # define helper function
@@ -56,12 +76,14 @@ install_packages <-
     # install all packages in the lst from CRAN
     sapply(pkgs_list, utils::install.packages, character.only = TRUE)
 
-    # install RFossilpol from GitHub
-    # devtools::install_github(
-    #  "HOPE-UIB-BIO/R-Fossilpol-package",
-    #  quiet = FALSE,
-    #  upgrade = FALSE
-    )
+     #install 'RFossilpol' from GitHub
+     remotes::install_github(
+      "HOPE-UIB-BIO/R-Fossilpol-package"
+      )
+     
+     # Install 'REcopol' from FitHub
+     remotes::install_github(
+       "HOPE-UIB-BIO/R-Ecopol-package")
   }
 
 #----------------------------------------------------------#
@@ -92,30 +114,15 @@ install_packages(package_list)
 library(here)
 
 # if there is no lock file present make a new snapshot
-if
-(
+if (
   isFALSE("library_list.lock" %in% list.files(here::here("renv")))
-) {
+  ) {
   renv::snapshot(lockfile = here::here("renv/library_list.lock"))
 } else {
   renv::restore(lockfile = here::here("renv/library_list.lock"))
 }
 
-#----------------------------------------------------------#
-# Step 8: GitHub hook
-#----------------------------------------------------------#
-
-# Prevent commiting to the Main
-usethis::use_git_hook(
-  hook = "pre-commit",
-  script = '#!/bin/sh
-  branch="$(git rev-parse --abbrev-ref HEAD)"
-  if [ "$branch" = "main" ]; then
-  echo "You cannot commit directly to main branch. Please make a new branch"
-  exit 1
-  fi'
-)
 
 #----------------------------------------------------------#
-# Step 9: Run the project 
+# Step 8: Run the project 
 #----------------------------------------------------------#
