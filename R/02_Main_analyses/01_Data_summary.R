36df91ee3d6422a43d0e8a7c3623de5c8a22e194
Kuber Bhatta <47561250+kbh022@users.noreply.github.com>
2024-07-02 22:59:01 +0200

    output summary

--- R/02_Main_analyses/01_Data_summary.R
+++ R/02_Main_analyses/01_Data_summary.R
@@ -1,6 +1,14 @@
 #----------------------------------------------------------#
+# Fossil pollen data can predict robust spatial patterns of biodiversity 
+#                        in the past
 #
-#       Latitudinal analysis of phylogenetic dispersion
+#                         K. Bhatta 
+#
+#                           2024
+#----------------------------------------------------------#
+
+#----------------------------------------------------------#
+#
 #                 Data output summary ----
 #
 #----------------------------------------------------------#
@@ -13,7 +21,7 @@ source("R/00_Config_file.R")
 #--------------------------------------------------------#
 # Extract_summary
 n_taxa_surface_samples <- 
-  read_rds("Inputs/Data/surface_pollen_filtered_030124.rds") %>% 
+  readr::read_rds("Inputs/Data/surface_pollen_filtered_030124.rds") %>% 
   dplyr::select(
     -c(sample_id, 
        lat, 
@@ -24,13 +32,13 @@ n_taxa_surface_samples <-
        climate_zone_revised)
     ) %>% 
   colnames(.) %>%
-  enframe(name = NULL,
+  tibble::enframe(name = NULL,
           value = "taxa") %>%
-  distinct() %>% 
+  dplyr::distinct() %>% 
   nrow(.)
 
 n_taxa_top_fossil_pollen_500yr <- 
-  read_rds("Inputs/Data/top_fossil_pollen_500yr_filtered_080124.rds") %>% 
+  readr::read_rds("Inputs/Data/top_fossil_pollen_500yr_filtered_080124.rds") %>% 
   dplyr::select(
     -c(dataset_id, 
        sample_id, 
@@ -42,39 +50,18 @@ n_taxa_top_fossil_pollen_500yr <-
        climate_zone_revised)
     ) %>% 
   colnames(.) %>%
-  enframe(name = NULL,
+  tibble::enframe(name = NULL,
           value = "taxa") %>%
-  distinct() %>% 
+  dplyr::distinct() %>% 
   nrow(.)
 
-n_taxa_top_fossil_pollen_1000yr <- 
-  read_rds("Inputs/Data/top_fossil_pollen_1000yr_filtered_080124.rds") %>% 
-  dplyr::select(
-    -c(
-      dataset_id,
-      sample_id,
-      lat,
-      long,
-      ecozone_koppen_5,
-      ecozone_koppen_15,
-      ecozone_koppen_30,
-      climate_zone_revised
-    )
-    ) %>% 
-  colnames(.) %>%
-  enframe(name = NULL,
-          value = "taxa") %>%
-  distinct() %>% 
-  nrow(.)
-
-
-phylo_div_full <- read_rds("Inputs/Data/phylo_div_full_090123.rds")
-turnover_combined <- read_rds("Inputs/Data/turnover_combined_050224.rds")
+phylo_div_full <- readr::read_rds("Inputs/Data/phylo_div_full_090123.rds")
+turnover_combined <- readr::read_rds("Inputs/Data/turnover_combined_050224.rds")
 
 surface_samples <- 
   phylo_div_full %>% 
   dplyr::filter(data_type == "surface_samples") %>% 
-  summarise(datasets = nrow(.),
+  dplyr::summarise(datasets = nrow(.),
             samples = nrow(.),
             mean_taxon_richness = mean(ntaxa),
             min_mpd = min(ses_mpd),
@@ -84,7 +71,7 @@ surface_samples <-
             max_mntd = max(ses_mntd),
             mean_mntd = mean(ses_mntd),
             data_type = unique(.$data_type)
-  ) %>% 
+            ) %>% 
   dplyr::ungroup() %>% 
   dplyr::mutate(n_taxa = paste(n_taxa_surface_samples))
 
@@ -94,10 +81,10 @@ top_500_yr_samples <-
   dplyr::mutate(datasets = phylo_div_full %>% 
                   dplyr::filter(data_type == "top_500_yr") %>% 
                   dplyr::select(dataset_id) %>% 
-                  distinct() %>% 
+                  dplyr::distinct() %>% 
                   nrow(.)
-  ) %>% 
-  summarise(
+                ) %>% 
+  dplyr::summarise(
     datasets = mean(datasets),
     samples = nrow(.),
     mean_taxon_richness = mean(ntaxa),
@@ -112,41 +99,15 @@ top_500_yr_samples <-
   dplyr::ungroup() %>%
   dplyr::mutate(n_taxa = paste(n_taxa_top_fossil_pollen_500yr))
 
-top_1000_yr_samples <- 
-  phylo_div_full %>% 
-  dplyr::filter(data_type == "top_1000_yr") %>% 
-  dplyr::mutate(datasets = phylo_div_full %>% 
-                  dplyr::filter(data_type == "top_1000_yr") %>% 
-                  dplyr::select(dataset_id) %>% 
-                  distinct() %>% 
-                  nrow(.)
-  ) %>% 
-  summarise(
-    datasets = mean(datasets),
-    samples = nrow(.),
-    mean_taxon_richness = mean(ntaxa),
-    min_mpd = min(ses_mpd),
-    max_mpd = max(ses_mpd),
-    mean_mpd = mean(ses_mpd),
-    min_mntd = min(ses_mntd),
-    max_mntd = max(ses_mntd),
-    mean_mntd = mean(ses_mntd),
-    data_type = unique(.$data_type)
-  ) %>%
-  dplyr::ungroup() %>%
-  dplyr::mutate(n_taxa = paste(n_taxa_top_fossil_pollen_1000yr))
-
 phylodiversity <- 
-  bind_rows(surface_samples,
-            top_500_yr_samples,
-            top_1000_yr_samples) %>% 
-  as_tibble()
+  dplyr::bind_rows(surface_samples,
+            top_500_yr_samples) %>% 
+  tibble::as_tibble()
+
 compositional_turnover <- 
   turnover_combined %>% 
   dplyr::mutate(data_type = ifelse(data_type == "fossil_pollen_top_500yr",
-                                   "top_500_yr", data_type),
-                data_type = ifelse(data_type == "fossil_pollen_top_1000yr",
-                                   "top_1000_yr", data_type)
+                                   "top_500_yr", data_type)
                 ) %>% 
   dplyr::select(data_type, axis_1) %>% 
   dplyr::group_by(data_type) %>% 
@@ -163,12 +124,12 @@ compositional_turnover <-
                                               )
                 ) %>% 
   dplyr::select(-data) %>% 
-  as_tibble()
+  tibble::as_tibble()
 
 
 output_summary <- 
   phylodiversity %>% 
   dplyr::left_join(compositional_turnover, by = "data_type")
 
-write_csv(output_summary,
-          file = "Outputs/Table/Data_outputs_summary_050224.csv")
+readr::write_csv(output_summary,
+                 file = "Outputs/Table/Data_outputs_summary_050224.csv")
